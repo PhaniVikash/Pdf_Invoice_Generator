b83612a55ec592ad76404fe3894661b6c4891af3
Vikash <phanivikash2@gmail.com>
2025-02-18 23:52:37 +0530

    Invoice pdf format

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 import pandas as pd
+from fpdf import FPDF
 import glob
+from pathlib import Path
+
 # glob is a standard module that helps to get all the files in a directory as a list
 filepaths=glob.glob("invoices/*.xlsx")
 
-for i in filepaths:
-    df=pd.read_excel(i,sheet_name="Sheet 1")
-    print(df)
+for filepath in filepaths:
+    df=pd.read_excel(filepath,sheet_name="Sheet 1")
+    pdf = FPDF(orientation="P",unit="mm",format="A4")
+    pdf.add_page()
+    filename=Path(filepath).stem
+    invoice_nr=filename.split("-")[0]
+    pdf.set_font(family="Times", style="B", size=18)
+    pdf.cell(w=0,h=12,txt=f"Invoice Number  : {invoice_nr}",align="L",border=0)
+    pdf.output(f"PDF's/{filename}.pdf")
